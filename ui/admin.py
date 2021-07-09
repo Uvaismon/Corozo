cd6de0a09cce59821ac60c3fcfde181997c984e5
Vignesh V <vinayv1357@gmail.com>
2021-07-09 19:27:49 +0530

    Admin control panel layout done

--- ui/admin.py
+++ ui/admin.py
@@ -19,25 +19,25 @@ class Admin:
 
         root = Tk()
         root.title('Admin Login')
-        root.geometry('400x400')
-        root.minsize(400, 200)
-        root.maxsize(400, 200)
+        root.geometry('400x300')
+        root.minsize(400, 300)
+        root.maxsize(400, 300)
         login = Label(root, text="Log In")
         login.config(font=(44))
         login.grid(pady=(20, 0), padx=(170, 0), columnspan=3)
 
         admin_id = Label(root, text='Admin Id : ')
-        admin_id_entry = Entry(root)
+        admin_id_entry = Entry(root, width=30)
         admin_id.grid(row=3, pady=(40, 10), column=0, sticky=E)
         admin_id_entry.grid(row=3, pady=(40, 10), column=1, sticky=S)
 
         admin_pass = Label(root, text='Password : ')
-        admin_pass_entry = Entry(root)
+        admin_pass_entry = Entry(root, width=30)
         admin_pass.grid(row=4, column=0, sticky=E)
         admin_pass_entry.grid(row=4, column=1, sticky=S)
 
         login_btn = Button(root, text='Login', command=log_in_admin)
-        login_btn.grid(row=5, column=1, padx=(80, 0), pady=20)
+        login_btn.grid(row=5, column=1, padx=(140, 0), pady=20)
         root.mainloop()
 
     @staticmethod
@@ -61,6 +61,26 @@ class Admin:
 
         def change_password_window():
             pass
+
+        root = Tk()
+        root.title('Admin Control Panel')
+        root.geometry('400x300')
+        root.minsize(400, 300)
+        root.maxsize(400, 300)
+
+        create_account = Button(root,text='Create New Account', padx=35, command=create_new_account_window)
+        create_account.grid(pady=(100, 0), padx=(10,0), row=1,column=1, sticky=W)
+
+        search_transact = Button(root, text='Search Transactions', padx=28, command=search_transaction_window)
+        search_transact.grid(pady=(100,0), padx=(10,0), row=1, column=2,sticky=W)
+
+        add_money = Button(root, text='Add Money', padx=59, command=add_money_window)
+        add_money.grid(pady=(20,0), padx=(10,0), row=2, sticky=W, column=1)
+
+        change_pass= Button(root, text='Change Passwords', padx=30, command=change_password_window)
+        change_pass.grid(pady=(20,0), padx=(10,0), row=2, sticky=W, column=2)
+        root.mainloop()
+
 
     @staticmethod
     def new_account():
@@ -107,3 +127,4 @@ if __name__ == '__main__':
     If you have to debug and test any of the CorozoUI class methods, please do it in this block.
     """
     Admin.admin()
+    Admin.admin_control_panel()
