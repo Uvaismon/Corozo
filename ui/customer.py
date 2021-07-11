651f456cd1fdecc779403ad3817054a2437b0d7c
zabiullasheriff <zabi.lfc@gmail.com>
2021-07-11 20:06:01 +0530

    home,searchTransaction,settings,changepass,closeaccount,send money added

--- ui/customer.py
+++ ui/customer.py
@@ -2,6 +2,7 @@ import datetime
 import tkinter as tk
 from tkinter import *
 
+
 class Customer:
 
     def __init__(self):
@@ -43,7 +44,6 @@ class Customer:
         Passcode.grid(row=2, column=0)
         e2.grid(row=2, column=1, padx=10, pady=10)
 
-
         my_button = Button(root, text="LOGIN", command=log_in)
         my_button.grid(row=3, column=1)
         # welcome.grid(row=0, column=0)
@@ -73,7 +73,44 @@ class Customer:
         def settings():
             pass
 
+        root = Tk()
+        root.geometry("400x400")
+        root.minsize(400, 400)
+        root.maxsize(400, 400)
+        root.title("Home")
 
+        AccountNumber = Label(root, text="Enter Account Number")
+
+        e1 = Label(root, text=account_number, width=30)
+
+        Accountholdername = Label(root, text="Enter Name: ")
+        e2 = Label(root, text=acct_holder_name, width=30)
+
+        AccountNumber.grid(row=1, column=0)
+        e1.grid(row=1, column=1, padx=10, pady=10)
+
+        Accountholdername.grid(row=2, column=0)
+        e2.grid(row=2, column=1, padx=10, pady=10)
+
+        Accounttype = Label(root, text="Choose account type")
+
+        e3 = Label(root, text=acct_type, width=30)
+
+        AccountBalance = Label(root, text="The balance is: ")
+        e4 = Label(root, text=balance, width=30)
+
+        Accounttype.grid(row=3, column=0)
+        e3.grid(row=3, column=1, padx=10, pady=10)
+
+        AccountBalance.grid(row=4, column=0)
+        e4.grid(row=4, column=1, padx=10, pady=10)
+
+        transact = Button(root, text="Transact", command=transact)
+        transact.grid(row=6, column=0)
+        settings = Button(root, text="Settings", command=settings)
+        settings.grid(row=6, column=1)
+
+        root.mainloop()
 
     @staticmethod
     def settings():
@@ -90,6 +127,25 @@ class Customer:
 
         def close_account_window():
             pass
+
+        root = Tk()
+        root.geometry("400x400")
+        root.minsize(400, 200)
+        root.maxsize(400, 200)
+
+        def Change_password():
+            Change_password()
+
+        def close_account():
+            close_account()
+
+        Change_password = Button(root, text="Change Password", command=Change_password)
+        Change_password.grid(row=1, column=0, padx=20, pady=15)
+
+        close_account = Button(root, text="Close Account", command=close_account)
+        close_account.grid(row=2, column=0, padx=20, pady=15)
+
+        root.mainloop()
 
     @staticmethod
     def transact():
@@ -110,6 +166,22 @@ class Customer:
         def send_money_window():
             pass
 
+        root = Tk()
+        root.geometry("400x400")
+        root.minsize(400, 200)
+        root.maxsize(400, 200)
+
+        account_statement = Button(root, text="Account Statement", command=account_statement)
+        account_statement.grid(row=1, column=0, padx=20, pady=15)
+
+        search_transaction_window = Button(root, text="Search Transaction", command=search_transaction_window)
+        search_transaction_window.grid(row=2, column=0, padx=20, pady=15)
+
+        send_money_window = Button(root, text="Send Money", command=send_money_window)
+        send_money_window.grid(row=3, column=0, padx=20, pady=15)
+
+        root.mainloop()
+
     @staticmethod
     def change_password():
         """
@@ -120,6 +192,35 @@ class Customer:
 
         def change_password():
             pass
+
+        root = Tk()
+        root.geometry("400x400")
+        root.minsize(400, 200)
+        root.maxsize(400, 200)
+
+        enter_old_passcode = Label(root, text="Enter Old Passcode")
+
+        e = Entry(root, width=30)
+
+        enter_new_passcode = Label(root, text="Enter New Passcode: ")
+        e2 = Entry(root, width=30)
+
+        reenter_new_passcode = Label(root, text="Re-enter New Passcode")
+        e3 = Entry(root, width=30)
+
+        enter_old_passcode.grid(row=1, column=0)
+        e.grid(row=1, column=1, padx=10, pady=10)
+
+        enter_new_passcode.grid(row=2, column=0)
+        e2.grid(row=2, column=1, padx=10, pady=10)
+
+        reenter_new_passcode.grid(row=3, column=0)
+        e3.grid(row=3, column=1, padx=10, pady=10)
+
+        submit = Button(root, text="Submit", command=change_password)
+        submit.grid(row=4, column=0, padx=30, pady=15)
+
+        root.mainloop()
 
     @staticmethod
     def close_account(acct_number: int, balance: float):
@@ -133,6 +234,38 @@ class Customer:
 
         def close_account():
             pass
+
+        root = Tk()
+
+        Account_Number = 12345
+        Account_Balance = 1220
+        password = "xyzxyz"
+
+        AccountNumber = Label(root, text="Enter Account Number")
+
+        e1 = Label(root, text=Account_Number, width=30)
+
+        AccountBalance = Label(root, text="Account Balance is : ")
+        e2 = Label(root, text=Account_Balance, width=30)
+
+        Password = Label(root, text="Enter Password")
+        e3 = Entry(root, width=30)
+
+        AccountNumber.grid(row=1, column=0)
+        e1.grid(row=1, column=1, padx=10, pady=10)
+
+        AccountBalance.grid(row=2, column=0)
+        e2.grid(row=2, column=1, padx=10, pady=10)
+
+        Password.grid(row=3, column=0)
+        e3.grid(row=3, column=1, padx=10, pady=10)
+
+
+
+        close_account = Button(root, text="Close", command=close_account)
+        close_account.grid(row=4, column=0, padx=30, pady=15)
+
+        root.mainloop()
 
     @staticmethod
     def account_statement(acct_number: int, acct_type: str, trans_list: list):
@@ -169,6 +302,28 @@ class Customer:
         def send_money():
             pass
 
+        root = Tk()
+
+
+
+        AccountNumber = Label(root, text="Enter Account Number")
+
+        e1 = Entry(root, width=30)
+
+        Amount = Label(root, text="Enter Amount")
+
+        e2 = Entry(root, width=30)
+
+        AccountNumber.grid(row=1, column=0)
+        e1.grid(row=1, column=1, padx=10, pady=10)
+
+        Amount.grid(row=2, column=0)
+        e2.grid(row=2, column=1, padx=10, pady=10)
+
+        send = Button(root, text="Send", command=send_money)
+        send.grid(row=3, column=0, padx=10, pady=10)
+
+        root.mainloop()
 
 if __name__ == '__main__':
     """
@@ -176,3 +331,9 @@ if __name__ == '__main__':
     """
 
     Customer.log_in()
+    Customer.home(1234, "zabi", "Savings", 5000)
+    Customer.settings()
+    Customer.transact()
+    Customer.change_password()
+    Customer.close_account(1234, 2000)
+    Customer.send_money()
