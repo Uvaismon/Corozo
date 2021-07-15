899631d08936d1fcb2db29327330af54b3b6fcd2
Uvaismon <uvaisullas@gmail.com>
2021-07-15 17:54:29 +0530

    Constants related to customer data file is added.

--- constants/__init__.py
+++ constants/__init__.py
@@ -3,3 +3,5 @@ import os
 DATA_DIRECTORY = os.path.join(r'D:\Python\Corozo', 'app_data')
 META_DIRECTORY = os.path.join(DATA_DIRECTORY, 'meta')
 CUSTOMER_DATA_DIRECTORY = os.path.join(DATA_DIRECTORY, 'customer_data', 'data')
+CUSTOMER_META_DIRECTORY = os.path.join(DATA_DIRECTORY, 'customer_data', 'index')
+BLOCK_SIZE = 10
